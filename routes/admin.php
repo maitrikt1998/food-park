8c9a65a97a528db4a799d93f93e07bac43180519
Maitrik <Maitrikt1998@gmail.com>
2024-11-06 09:07:12 +0530

    admin panel changes

--- routes/admin.php
+++ routes/admin.php
@@ -2,8 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\AdminDashboardController;
-
+use App\Http\Controllers\Admin\ProfileController;
 
 Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
     Route::get('dashboard', [AdminDashboardController::class, 'index'])->name('dashboard');
+
+    Route::get('/profile',[ProfileController::class,'index'])->name('profile');
 });
